6dd6f178ce26523b05bc85bc5ad2f30fe7b5fbc6
Bhavya L Narayanan <bhavyaln02@gmail.com>
2021-05-09 19:41:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/driveeasy/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.driveeasy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageView logo;
+    TextView slogan;
+    Animation topAnim,bottomAnim;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_anim);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_anim);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+        int DELAY = 5000;
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        }, DELAY);
+    }
+}
