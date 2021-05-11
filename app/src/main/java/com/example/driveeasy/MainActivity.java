190a6e401fe6f3a3d9da07612d9448d18e0cf53f
Bhavya L Narayanan <bhavyaln02@gmail.com>
2021-05-11 21:18:03 +0530

    XML Sign in Commit

--- app/src/main/java/com/example/driveeasy/MainActivity.java
+++ app/src/main/java/com/example/driveeasy/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(MainActivity.this, Dashboard.class);
+                Intent intent = new Intent(MainActivity.this, SignIn.class);
                 startActivity(intent);
                 finish();
             }
