a3482a29005ffd1ac79889af9716acd5354754c2
anishsreenivas82 <77966360+anishsreenivas82@users.noreply.github.com>
2021-05-14 15:52:50 +0530

    Dashboard

--- app/src/main/java/com/example/driveeasy/Dashboard.java
+++ app/src/main/java/com/example/driveeasy/Dashboard.java
@@ -1,14 +1,79 @@
 package com.example.driveeasy;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.os.Build;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class Dashboard extends AppCompatActivity {
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        toolbar=findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+
         setContentView(R.layout.activity_dashboard2);
+        drawerLayout=findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView nav_view= (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if(id==R.id.home)
+                {
+                    Toast.makeText(Dashboard.this,"Home",Toast.LENGTH_SHORT).show();
+
+                }
+                else if(id==R.id.logout)
+                {
+                    Toast.makeText(Dashboard.this,"Logout",Toast.LENGTH_SHORT).show();
+
+                }
+
+                return true;
+            }
+        });
+
+
+}
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
