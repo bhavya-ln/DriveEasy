c7944ebf1401e99b728b1d6e2a2313874c184b8e
pranavsrikanth <72306130+pranavsrikanth@users.noreply.github.com>
2021-05-15 23:39:28 +0530

    calendar
    added date picker

--- app/src/main/java/com/example/driveeasy/Dashboard.java
+++ app/src/main/java/com/example/driveeasy/Dashboard.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
 import android.widget.SearchView;
 import android.widget.Toast;
 import android.widget.Toolbar;
@@ -21,18 +25,27 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class Dashboard extends AppCompatActivity {
     ActionBarDrawerToggle toggle;
     DrawerLayout drawerLayout;
     Toolbar toolbar;
+    Calendar calendar;
+    public Button proceed;
 
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard2);
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onStart() {
+        super.onStart();
         toolbar=findViewById(R.id.toolbar);
+        proceed=findViewById(R.id.Proceed);
         setActionBar(toolbar);
         NavigationView nav_view= (NavigationView)findViewById(R.id.nav_view);
         nav_view.bringToFront();
@@ -40,6 +53,17 @@ public class Dashboard extends AppCompatActivity {
         toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.Open,R.string.Close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                if(calendarView.getDate()<System.currentTimeMillis()){
+                    calendarView.setMinDate(System.currentTimeMillis());
+                }
+            }
+        });
+
         nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -48,7 +72,6 @@ public class Dashboard extends AppCompatActivity {
                 if(id==R.id.home)
                 {
                     Toast.makeText(Dashboard.this,"Home",Toast.LENGTH_SHORT).show();
-
                 }
                 else if(id==R.id.logout)
                 {
@@ -62,6 +85,7 @@ public class Dashboard extends AppCompatActivity {
                 return true;
             }
         });
+
 
 
 }
@@ -85,6 +109,12 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
+    }
+    private void reload() {
+        Intent intent = new Intent(Dashboard.this, Dashboard.class);
+        startActivity(intent);
+        finish();
+
     }
 }
 
