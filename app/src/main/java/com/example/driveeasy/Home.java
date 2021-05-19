6997d31b1a3967b178035d4b2c4dee5b9d3a18a2
pranavsrikanth <72306130+pranavsrikanth@users.noreply.github.com>
2021-05-19 12:49:51 +0530

    search bar in home
    rough first draft. works but redo ui a bit

--- app/src/main/java/com/example/driveeasy/Home.java
+++ app/src/main/java/com/example/driveeasy/Home.java
@@ -1,11 +1,20 @@
 package com.example.driveeasy;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -14,6 +23,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -29,6 +39,17 @@ public class Home extends AppCompatActivity {
     CarAdapter myAdapter;
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle toggle;
+    ListView listView;
+
+    // creating a new array list.
+
+
+    // creating a variable for database reference.
+    FirebaseDatabase rootNode;
+    DatabaseReference refdb;
+
+    ArrayAdapter arrayAdapter;
+    ArrayList<String> arrayList = new ArrayList<>();
 
 
 
@@ -36,6 +57,49 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        listView = findViewById(R.id.list);
+        rootNode = FirebaseDatabase.getInstance();
+        refdb =  rootNode.getReference("CarNames");
+
+
+        //        listView = findViewById(R.id.list);
+        //        coursesArrayList = new ArrayList<String>();
+        //
+        //        // calling a method to get data from
+        //        // Firebase and set data to list view
+        //        initializeListView();
+
+        listView = findViewById(R.id.list);
+        refdb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue(String.class);
+                arrayList.add(value);
+                arrayAdapter = new ArrayAdapter<String>(com.example.driveeasy.Home.this,android.R.layout.simple_list_item_1,arrayList);
+                listView.setAdapter(arrayAdapter);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         recyclerView = findViewById(R.id.recylerView);
         reference = FirebaseDatabase.getInstance().getReference("Cars");
         recyclerView.setHasFixedSize(true);
@@ -96,6 +160,38 @@ public class Home extends AppCompatActivity {
                 return true;
             }
         });
+    }
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Enter Car Name");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(com.example.driveeasy.Home.this,query,Toast.LENGTH_SHORT).show();
+                listView.setVisibility(View.GONE);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                listView.setVisibility(View.VISIBLE);
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        Object listItem = listView.getItemAtPosition(position);
+                        searchView.setQuery(listItem.toString(),true);
+                    }
+                });
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+
+            }
+        });
+        return true;
     }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
