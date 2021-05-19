bab70429b316bd3d01a2aba690aa45a211112f70
pranavsrikanth <72306130+pranavsrikanth@users.noreply.github.com>
2021-05-19 12:28:19 +0530

    nav drawable in home screen
    just as name suggests lol

--- app/src/main/java/com/example/driveeasy/Home.java
+++ app/src/main/java/com/example/driveeasy/Home.java
@@ -1,12 +1,19 @@
 package com.example.driveeasy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,6 +27,8 @@ public class Home extends AppCompatActivity {
     DatabaseReference reference;
     ArrayList<Car> cars;
     CarAdapter myAdapter;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
 
 
 
@@ -50,5 +59,40 @@ public class Home extends AppCompatActivity {
 
             }
         });
+
+        NavigationView nav_view= (NavigationView)findViewById(R.id.nav_view);
+        nav_view.bringToFront();
+        drawerLayout=findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout, R.string.Open, R.string.Close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+                if(id== R.id.home)
+                {
+                    Toast.makeText(com.example.driveeasy.Home.this,"Home",Toast.LENGTH_SHORT).show();
+                }
+                else if(id== R.id.logout)
+                {
+                    Toast.makeText(com.example.driveeasy.Home.this,"Logout",Toast.LENGTH_SHORT).show();
+                    FirebaseAuth.getInstance().signOut();
+                    Intent intent = new Intent(com.example.driveeasy.Home.this, SignIn.class);
+                    startActivity(intent);
+
+                }
+
+
+
+                return true;
+            }
+        });
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        return toggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 }
